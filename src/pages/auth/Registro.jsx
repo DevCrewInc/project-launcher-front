4282c142907d52ffa2d1b4f7f319c18c3cfac762
lilianaromerop <lilianaromerop11@gmail.com>
2021-11-25 16:58:16 -0500

    formulario de mentira

--- src/pages/auth/Registro.jsx
+++ src/pages/auth/Registro.jsx
@@ -5,17 +5,18 @@ import useFormData from 'hooks/useFormData';
 const Registro = () => {
 
     const{ form, formData, updateFormData} = useFormData();
-
     const submitForm = (e) => {
       e.preventdefault();
     }
 
   return (
     <>
-    <form onSubmit={submitForm} onChange={updateFormData} ref={form}>
-
-      <button className="bg-red-400 p-3">hola</button>
-    </form>
+      <form className="flex flex-col" onSubmit={submitForm} onChange={updateFormData} ref={form}>
+        <input className="bg-gray-100 rounded-full py-4 m-4"></input>
+        <input className="bg-gray-100 rounded-full py-4 m-4"></input>
+        <input className="bg-gray-100 rounded-full py-4 m-4"></input>
+        <button className="bg-red-400 rounded-full p-3 m-4">hola</button>
+      </form>
     </>
   );
 };
