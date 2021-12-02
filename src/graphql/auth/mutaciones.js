ed15fb1e3310609870300d50f5ec813820640696
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-12-02 19:49:43 +0000

    agregar revisiones de Daniel

--- src/graphql/auth/mutaciones.js
+++ src/graphql/auth/mutaciones.js
@@ -1,45 +1,37 @@
-import {gql} from '@apollo/client';
+import { gql } from '@apollo/client';
 
 const REGISTRO = gql`
- mutation Registro(
-    $nombre: String!,
-    $identificacion: String!,
-    $correo: String!,
-    $rol: Enum_Rol!,
-    $contrasena: String!,
-    $facultad:Enum_Facultad
-    $semestre:Enum_Semestre
-    ) 
-    
-    {
-  registro(
-    nombre: $nombre, 
-    identificacion: $identificacion, 
-    correo: $correo,
-    rol: $rol, 
-    contrasena: $contrasena
-    semestre:$semestre
-    facultad:$facultad
-    ){
+  mutation Registro(
+    $nombre: String!
+    $identificacion: String!
+    $correo: String!
+    $rol: Enum_Rol!
+    $contrasena: String!
+    $facultad: Enum_Facultad
+    $semestre: Enum_Semestre
+  ) {
+    registro(
+      nombre: $nombre
+      identificacion: $identificacion
+      correo: $correo
+      rol: $rol
+      contrasena: $contrasena
+      semestre: $semestre
+      facultad: $facultad
+    ) {
       token
       error
     }
-}
+  }
 `;
 
 const LOGIN = gql`
-  mutation login(
-    $correo: String!,
-    $contrasena:String!,
-    ){
-    login(
-      correo: $correo,
-      contrasena: $contrasena
-      ){
-        token
-        error
+  mutation login($correo: String!, $contrasena: String!) {
+    login(correo: $correo, contrasena: $contrasena) {
+      token
+      error
     }
-}`;
+  }
+`;
 
-export {REGISTRO,LOGIN};
-
+export { REGISTRO, LOGIN };
