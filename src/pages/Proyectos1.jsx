e9bf8de5ec93e54be9ef4aae76f7cdecfc8286cc
lilianaromerop <lilianaromerop11@gmail.com>
2021-12-02 15:07:57 -0500

    colores texto tablas

--- src/pages/Proyectos1.jsx
+++ src/pages/Proyectos1.jsx
@@ -32,7 +32,9 @@ const Proyectos1 = () => {
                             explicabo id, fugiat nesciunt et rerum quis. Quam enim sint cupiditate iusto repellendus
                             explicabo id.
                         </p>
-                        <Tabs tab1="Objetivos Generales"/>
+                        <div className="font-medium mt-12">
+                            <span className=" text-lg">TABS Objetivos Generales</span>
+                        </div>
                         <div>
                             <p className = "mt-6"> 
                                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit harum, commodi ullam repellendus, 
@@ -40,9 +42,6 @@ const Proyectos1 = () => {
                                 explicabo id,fugiat nesciunt et rerum quis. Quam enim sint cupiditate iusto repellendus
                                 explicabo id.
                             </p>
-                        </div>
-                        <div className="font-medium mt-12">
-                            <span className=" text-lg">Avances</span>
                         </div>
                     </div> 
                 </div>
@@ -61,7 +60,7 @@ const Proyectos1 = () => {
                             
 
                         </div>
-                    <div className="overflow-y-auto h-80 mt-10">
+                    <div className="overflow-y-auto h-30 mt-10">
                         {/* TRIPULANTES */}
                         <div className = "border-tripulantes flex items-center pb-4">
                                 <img src={fotoman} className = "rounded-full w-12 mr-4"/>
@@ -74,24 +73,62 @@ const Proyectos1 = () => {
                         </div>
                         {/* SOLICITUDES */}
                         <div className="mt-8">
-                            <div className = "border-tripulantes items-center py-4">
-                                <div className="flex">
-                                    <img src={fotoman} className = "rounded-full w-12 h-12 mr-4"/>
-                                    <div className = "flex flex-col">
-                                        <span className = "font-semibold text-sm">Juan Camilo Pérez</span>
-                                        <span className = "font-light text-sm">Ingenieria </span>
-                                            <div className="space-x-4 flex mt-4">
-                                                <button className="px-4 h-7 outlined-button-perfil">RECHAZAR</button>
-                                                <button className="px-4 h-7 filled-button-perfil">ACEPTAR</button>
-                                            </div>
+                        <div className = "border-tripulantes items-center py-4">
+                            <div className="flex">
+                                <img src={fotoman} className = "rounded-full w-12 h-12 mr-4"/>
+                                <div className = "flex flex-col">
+                                    <span className = "font-semibold text-sm">Juan Camilo Pérez</span>
+                                    <span className = "font-light text-sm">Ingenieria </span>
+                                        <div className="space-x-4 flex mt-4">
+                                            <button className="px-4 h-7 outlined-button-perfil">RECHAZAR</button>
+                                            <button className="px-4 h-7 filled-button-perfil">ACEPTAR</button>
                                         </div>
                                     </div>
                                 </div>
-                                
                             </div>
+                        </div>
                         </div>
                     </div>
                 </div>
+
+
+                <div className = "flex flex-col">
+                        <div className="font-medium mt-12">
+                            <span className=" text-lg">Avances</span>
+                        </div>
+                        <div className="mt-6">
+                            <table class="table-auto text-center ">
+                                <thead>
+                                    <tr className="thead-color encabezado-tablas leading-10">
+                                        <th class="w-1/5">ID Avance</th>
+                                        <th class="w-1/5">Nombre Avance</th>
+                                        <th class="w-1/5">Fecha</th>
+                                        <th class="w-1/5">Responsable</th>
+                                        <th class="w-1/5">Revisión</th>
+                                        <th class="w-1/5 pr-6">Acciones</th>
+                                        
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr className="text-center leading-10 texto-tablas">
+                                        <td>Intro to CSS</td>
+                                        <td>Adam</td>
+                                        <td>858</td>
+                                        <td>858</td>
+                                        <td><i class="far fa-check-circle"></i></td>
+                                        <td class="pr-6 flex">
+                                            <i className = "fas fa-eye m-1 p-1 text-gray-400 hover:text-blue-600 cursor-pointer"/>
+                                            <i className = "fas fa-pen my-1 p-1 text-gray-400 hover:text-yellow-400 cursor-pointer"/>
+                                            <i className = "fas fa-trash my-1 p-1 text-gray-400 hover:text-red-400 cursor-pointer"/>
+                                        </td>
+                                    </tr>
+                                   
+                                </tbody>
+                                </table>
+
+                        </div>
+
+                    </div>
             </div>
         </div>
     )
